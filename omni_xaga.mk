2de5771e0b5208abe85affbc58ccf19581fecdf0
Kevin chinedu <kelvinchinedu97@gmail.com>
2024-11-12 22:50:39 +0100

    Update and rename twrp_xaga.mk to omni_xaga.mk

--- omni_xaga.mk
+++ omni_xaga.mk
@@ -22,14 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from our custom product configuration.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Device specific configs.
 $(call inherit-product, device/xiaomi/xaga/device.mk)
 
 ## Device identifier. This must come after all inclusions.
 PRODUCT_DEVICE := xaga
-PRODUCT_NAME := twrp_xaga
+PRODUCT_NAME := omni_xaga
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := xaga
 PRODUCT_MANUFACTURER := Xiaomi
